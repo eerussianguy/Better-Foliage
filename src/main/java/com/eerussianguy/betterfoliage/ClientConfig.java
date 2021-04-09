dad4db7e84cc8c46e401d241f771503742299fda
Oscar N <PaintNinja@users.noreply.github.com>
2021-04-09 17:22:31 +0100

    leavesVariationDistance, slightly increased default

--- src/main/java/com/eerussianguy/betterfoliage/ClientConfig.java
+++ src/main/java/com/eerussianguy/betterfoliage/ClientConfig.java
@@ -5,6 +5,7 @@ import java.util.function.Function;
 import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
 import net.minecraftforge.common.ForgeConfigSpec.Builder;
 import net.minecraftforge.common.ForgeConfigSpec.IntValue;
+import net.minecraftforge.common.ForgeConfigSpec.DoubleValue;
 
 import static com.eerussianguy.betterfoliage.BetterFoliage.MOD_ID;
 
@@ -18,6 +19,7 @@ public class ClientConfig
     public final BooleanValue snowballs;
 
     public final IntValue leavesCacheSize;
+    public final DoubleValue leavesVariationDistance;
 
     public final BooleanValue forceForgeLighting;
 
@@ -32,7 +34,8 @@ public class ClientConfig
         souls = builder.apply("souls").comment("Enable Souls?").define("souls", true);
         leaves = builder.apply("leaves").comment("Enable Leaves?").define("leaves", true);
         snowballs = builder.apply("snowballs").comment("Enable Snowballs?").define("snowballs", true);
-        leavesCacheSize = builder.apply("leavesCacheSize").comment("Determines the size of the leaves cache. Number of models cached per leaf block will be the number you input to the third power. Bigger cache = more RAM, but less z-fighting").worldRestart().defineInRange("leavesCacheSize", 7, 5, 20);
+        leavesCacheSize = builder.apply("leavesCacheSize").comment("Determines the size of the leaves cache. Number of models cached per leaf block will be the number you input to the third power. Bigger cache = more RAM, but more variation and less z-fighting as a result").worldRestart().defineInRange("leavesCacheSize", 7, 5, 20);
+        leavesVariationDistance = builder.apply("leavesVariationDistance").comment("Determines the max distance leaves block fluff can deviate from the actual block. 0.0 means no distance variation (all fluff is in the middle of the block)").worldRestart().defineInRange("leavesVariationDistance", 2.75f, 0f, 7f);
         forceForgeLighting = builder.apply("forceForgeLighting").comment("Force Forge Lighting Pipeline? (should be true when not using Optifine)").define("forceForgeLighting", true);
 
         innerBuilder.pop();
