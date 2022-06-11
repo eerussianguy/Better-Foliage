50000c1e0d2732f250d5fc329983dd13f5c19b20
EERussianguy <eerussianguy@gmail.com>
2022-06-11 13:20:09 -0400

    change to modern displayTest (also publish build that was built correctly)

--- src/main/java/com/eerussianguy/betterfoliage/BetterFoliage.java
+++ src/main/java/com/eerussianguy/betterfoliage/BetterFoliage.java
@@ -3,8 +3,6 @@ package com.eerussianguy.betterfoliage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 
@@ -19,8 +17,6 @@ public class BetterFoliage
 
     public BetterFoliage()
     {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> "Nothing", (remote, isServer) -> true));
-
         if (FMLEnvironment.dist == Dist.CLIENT)
         {
             EventHandler.init();
